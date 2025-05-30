8fa125869c3b56f95696728d7b40297451de9d25
Eliakim-Ellie <eliakimellie123@gmail.com>
2025-05-30 19:30:46 +0300

    Add initial portfolio files

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for reaching out! We'll get back to you shortly.");
+});
+Document.getElementById(submit).addEventListener("click", function () {
+
+})
